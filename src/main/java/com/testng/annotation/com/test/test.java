89308491adbb4cae67746c927800c6a4bbff300e
Rohit Bhosale <rohitb13@kpit.com>
2020-07-02 14:37:24 +0530

    third commit

--- src/main/java/com/testng/annotation/com/test/test.java
+++ src/main/java/com/testng/annotation/com/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Test");
+		System.out.println("Hello Test1");
 
 	}
 
